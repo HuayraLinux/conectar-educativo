b8f2a56565c0f1e627cd9677000375651b39246d
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-05-09 14:49:36 -0300

    Agregando gaze para livereload y otros ajustes menores.

--- app/controllers/test.js
+++ app/controllers/test.js
@@ -7,6 +7,10 @@ export default Ember.Controller.extend({
       this.get('api').getVideos().then((data) => {
         console.log(data);
       });
+    },
+    abrirDevtools() {
+      let nw_gui = requireNode('nw.gui');
+      nw_gui.Window.get().showDevTools();
     }
   }
 });
