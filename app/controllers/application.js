298587c74de716c6d777030d65f855dc2313e431
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-06-09 10:37:37 -0300

    Agregando atajos de teclado.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -4,10 +4,26 @@ export default Ember.Controller.extend({
 
   disableBackspace: Ember.on("init", function() {
 
+    // Previene que se pueda usar BACKSPACE para regresar a la ruta anterior.
     $(document).on("keydown", function (event) {
       if (event.which === 8 && !$(event.target).is("input, textarea")) {
         event.preventDefault();
       }
+    });
+
+
+    // Permite abrir las developer tools pulsando CMD+I
+    $(document).keypress(function(event) {
+
+      if (event.which === 105 && (event.ctrlKey || event.metaKey) || (event.which === 19)) {
+        event.preventDefault();
+
+        let nw_gui = requireNode('nw.gui');
+        nw_gui.Window.get().showDevTools();
+        return false;
+      }
+
+      return true;
     });
 
   }),
