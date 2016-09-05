9525e79be78685d715ded6de5a8e3c4ca86d44a7
Ignacio Losiggio <iglosiggio@gmail.com>
2016-09-05 15:18:50 -0300

    Quitando el WM_CLASS porque me olvidé que master era electron
    This reverts commit 7a25a0224b2963184f560a9eff1b6229b6984feb.

--- app/initializers/wm-class.js
+++
@@ -1,9 +0,0 @@
-export function initialize(/* application */) {
-  // application.inject('route', 'foo', 'service:foo');
-  requireNode('nwjs-hack').set_wmclass('conectar-educativo', true);
-}
-
-export default {
-  name: 'wm-class',
-  initialize
-};
